8dd03be7b122894db3a919cc8e1927bed9654b4a
yajurkuma1r <yajurkumar06@gmail.com>
2026-03-27 18:30:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   output: "export",
   trailingSlash: true,
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 module.exports = nextConfig;
